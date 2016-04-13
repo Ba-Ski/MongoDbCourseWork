2c17ee6ce9c4e6dc881ac12cb5d9195ea9cd47e3
Ba-Ski <morbius95@mail.ru>
2016-04-13 19:48:03 +0300

    forgotten files were added

---
+++ MongoDbApplication/BlogContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDbApplication.Structure;
+
+namespace MongoDbApplication
+{
+    public class BlogContext
+    {
+        public const string CONNECTION_STRING_NAME = "mongodb://localhost";
+        public const string DATABASE_NAME = "blog";
+        public const string POSTS_COLLECTION_NAME = "posts";
+        public const string USERS_COLLECTION_NAME = "users";
+
+        // This is ok... Normally, these or the entire BlogContext
+        // would be put into an IoC container. We aren't using one,
+        // so we'll just keep them statically here as they are 
+        // thread-safe.
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static BlogContext()
+        {
+            //var connectionString = ConfigurationManager.ConnectionStrings[CONNECTION_STRING_NAME].ConnectionString;
+            _client = new MongoClient(CONNECTION_STRING_NAME);
+            _database = _client.GetDatabase(DATABASE_NAME);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Post> Posts
+        {
+            get { return _database.GetCollection<Post>(POSTS_COLLECTION_NAME); }
+        }
+
+        public IMongoCollection<User> Users
+        {
+            get { return _database.GetCollection<User>(USERS_COLLECTION_NAME); }
+        }
+    }
+}
---
+++ MongoDbApplication/RequestsToBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDbApplication.Structure;
+
+namespace MongoDbApplication
+{
+    class RequestsToBase
+    {
+        public static async Task insertUser(User user)
+        {
+            var blogContext = new BlogContext(); 
+            await blogContext.Users.InsertOneAsync(user);
+        }
+
+        public static async Task insertPost(Post post)
+        {
+            var blogContext = new BlogContext();
+            await blogContext.Posts.InsertOneAsync(post);
+        }
+
+        public static async Task<List<Post>> findPost(BsonDocument filter)
+        {
+            var blogContext = new BlogContext();
+            var posts = await blogContext.Posts.Find(filter).ToListAsync();
+            return posts;
+        }
+
+        public static async Task updatePerson(Post post)
+        {
+            var blogContext = new BlogContext();
+            var result = await blogContext.Posts.UpdateOneAsync(
+                new BsonDocument("author", "Vasya"),
+                post.ToBsonDocument());
+                //new BsonDocument
+                //{
+                //    {"title","knowledge"},
+                //    {"author", "Kate"},
+
+                //});
+            Console.WriteLine("Найдено по соответствию: {0}; обновлено: {1}",
+                result.MatchedCount, result.ModifiedCount);
+        }
+
+        public static async Task DeletePerson(BsonDocument doc)
+        {
+            var blogContext = new BlogContext();
+            var filter = doc;
+            await blogContext.Posts.DeleteOneAsync(filter);
+        }
+    }
+}
