0316f69959ef4d695d1259e7f695923040279592
FiNNicKK <iliya251295@mail.ru>
2016-05-12 04:17:49 +0300

    A new request for base

---
+++ AddictionMethods.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDbApplication.Structure;
+
+namespace MongoDbApplication
+{
+    class AddictionMethods
+    {
+        const string English = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?";
+        const string Russian = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,";
+        public static string convertEngToRus(string input)
+        {
+            var result = new StringBuilder(input.Length);
+            int index;
+            foreach (var symbol in input)
+                result.Append((index = Russian.IndexOf(symbol)) != -1 ? English[index] :
+                symbol);
+            return result.ToString();
+        }
+        public static async void getAggregateData()
+        {
+            var blogContext = new BlogContext();
+            var collection = blogContext.Users;
+            var userCollection = await collection.Aggregate()
+                .Match(new BsonDocument { { "nick", new BsonDocument { { "$gte", "xv" }, { "$lt", "xz" } } } })
+                .Group(new BsonDocument { { "_id", new BsonDocument { { "nick", "$nick" }, { "registerDate", "$registerDate" } } } })
+                .Project(new BsonDocument { { "Nick", "$_id.nick" }, { "rDate", "$_id.registerDate" } })
+                .ToListAsync();
+            foreach (var human in userCollection)
+            {
+                Console.WriteLine("Nick:\t" + human.GetValue("Nick"));
+                Console.WriteLine("rDate:\t" + human.GetValue("rDate"));
+                Console.WriteLine();
+            }
+        }
+        public static List<User> getUserByNameWithRegex(string input)
+        {
+
+            if (isEnglish(input))
+            {
+                input = convertEngToRus(input);
+            }
+
+            var blogContext = new BlogContext();
+            var collection = blogContext.Users;
+            Regex regex = new Regex(".*" + input + ".*");
+            var name = collection.Aggregate().Match(c => regex.IsMatch(c.name)).ToListAsync().Result;
+            return name;
+
+        }
+        public static bool isEnglish(string s)
+        {
+            s = s.ToLower();
+            byte[] b = System.Text.Encoding.Default.GetBytes(s);
+            int angl_count = 0, rus_count = 0;
+            foreach (byte bt in b)
+            {
+                if ((bt >= 97) && (bt <= 122)) angl_count++;
+                if ((bt >= 224) && (bt <= 255)) rus_count++;
+            }
+            if (angl_count > rus_count) return false;
+            else return true;
+        }
+        public static List<User> getUser(string s)
+        {
+            var blogContext = new BlogContext();
+            var collection = blogContext.Users;
+            var result = collection.Aggregate().Match(new BsonDocument { { "name", s } }).ToListAsync().Result;
+            return result;
+        }
+        public static void task2FindWithLevinstein()
+        {
+            Console.WriteLine("What user would you like to find?");
+            Dictionary<string, double> matches = new Dictionary<string, double>();
+            string line2 = Console.ReadLine();
+            if (isEnglish(line2))
+            {
+                line2 =convertEngToRus(line2);
+            }
+            var users2 = getUser(line2);
+            if ((users2 != null) && (users2.Count != 0))
+            {
+                foreach (var user in users2)
+                {
+                    Console.WriteLine("User with nick {1} and name {0} was registred at {2}", user.name, user.nick, user.registerDate);
+                }
+            }
+            else
+            {
+                double minmatch = 100;
+                var users3 = getUserByNameWithRegex(line2);
+                if ((users3 != null) && (users3.Count != 0))
+                {
+                    foreach (var user in users3)
+                    {
+                        if (!matches.ContainsKey(user.name))
+                        {
+                            double match = AlgoSearch.LevenshteinDist(user.name, line2);
+                            matches.Add(user.name, match);
+                        }
+                    }
+                }
+                if (matches.Count != 0)
+                {
+                    matches = matches.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+                    foreach (var i in matches)
+                    {
+                        if (i.Value < minmatch) minmatch = i.Value;
+                    }
+                    Console.WriteLine("Maybe you've mistaked and you wanted to find somebody from the list below ?");
+                    foreach (var i in matches)
+                    {
+                        if (i.Value == minmatch)
+                            Console.WriteLine("{0}", i.Key);
+                    }
+                }
+                else
+                    Console.WriteLine("Seems you've made and mistake and there are no one who can be similar to your entered name");
+            }
+        }
+
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDbApplication.Structure;
+using MongoDbApplication.Habr;
+
+namespace MongoDbApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //try {
+            //    var posts = HabrParser.parse();
+            //    using (StreamWriter sw = new StreamWriter("posts.json")) {
+            //        foreach (var item in posts.Item1)
+            //        {
+            //            RequestsToBase.insertPost(item).GetAwaiter().GetResult();
+            //            sw.WriteLine(item.ToJson());
+            //            sw.WriteLine();
+            //        }
+            //    }
+            //    using (StreamWriter sw = new StreamWriter("users.json"))
+            //    {
+            //        foreach (var item in posts.Item2)
+            //        {
+            //            RequestsToBase.insertUser(item).GetAwaiter().GetResult();
+            //            sw.WriteLine(item.ToJson());
+            //            sw.WriteLine();
+            //        }
+            //    }
+
+            // }
+            // catch(ApplicationException ex)
+            // {
+            //     Console.WriteLine("[Parsing error]: " + ex.Message);
+            // }
+            #region old one
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    RequestsToBase.insertUser(new User
+            //    {
+            //        name = "Вася" + i
+            //    }).GetAwaiter().GetResult();
+            //}
+            /*RequestsToBase.insertUser(new User
+            {
+                name = "arodygin",
+                email = "arodygin@yandex.ru",
+                password = "i am cool guy",
+            }).GetAwaiter().GetResult();
+
+            RequestsToBase.insertUser(new User
+            {
+                name = "sidristij",
+                email = "sidristij@yandex.ru",
+                password = "i am cool guy",
+            }).GetAwaiter().GetResult();
+
+            RequestsToBase.insertUser(new User
+            {
+                name = "kosatchev",
+                email = "kosatchev@yandex.ru",
+                password = "i am cool guy",
+            }).GetAwaiter().GetResult();*/
+
+            /*var posts = RequestsToBase.findPost(new BsonDocument
+            {
+                {"author" , "kosatchev" }
+            }).GetAwaiter().GetResult();
+
+            foreach (Post post in posts)
+            {
+                Console.WriteLine("{0} - {1} ({2})", post.Id, post.title, post.author);
+            }*/
+
+            /*RequestsToBase.insertUser(new User
+            {
+                name = "ITMO",
+                email = "ITMO@yandex.ru",
+                password = "i am cool guy",
+            }).GetAwaiter().GetResult();*/
+            #endregion
+            #region Iliya one
+
+            //AddictionMethods.getAggregateData();
+            //Console.WriteLine(AddictionMethods.convertEngToRus(Console.ReadLine()));
+            //Console.WriteLine("Enter a search pattern ");
+
+
+            // var userList = AddictionMethods.getUserByNameWithRegex(Console.ReadLine());
+            // if ((userList == null) || (userList.Count == 0))
+            //     Console.WriteLine("No users were found");
+            // else
+            //     foreach (var user in userList)
+            //     {
+            //         Console.WriteLine("Nick " + user.nick);
+            //         Console.WriteLine("rDatefsd " + user.registerDate);
+            //     }
+            #endregion
+            #region Task Algo Fuzzy search (1)
+            //Console.WriteLine("Enter a string pattern, that will be insert in \".*pattern.*\"");
+            //var users = AddictionMethods.getUserByNameWithRegex(Console.ReadLine());
+            //double maxMatch = 0;
+            //Console.WriteLine("Enter a string, that will be original for comparing with");
+            //string line = Console.ReadLine(); 
+            //Dictionary<string, double> matches = new Dictionary<string, double>();
+            //foreach (var user in users)
+            //{
+            //    if (!matches.ContainsKey(user.name)){
+            //        double match = AlgoSearch.LevenshteinDist(user.name, line);
+            //        if (maxMatch < match) maxMatch = match;
+            //        matches.Add(user.name, match);
+            //    }
+            //}
+            //if (matches.Count != 0){
+            //    foreach (var d in matches)
+            //        Console.WriteLine("Name: {0} distance: {1}", d.Key, d.Value);            //}
+            //else Console.WriteLine("There are no elements for comparing with");
+            #endregion
+
+            #region Task Algo Fuzzy search (2)
+            AddictionMethods.task2FindWithLevinstein();
+            #endregion
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
